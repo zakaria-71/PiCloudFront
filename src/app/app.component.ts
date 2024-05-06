65f8a8f34a67338c5ec935c8b2ec07aa66594c1c
sebtiranime <125137962+sebtiranime@users.noreply.github.com>
2024-05-07 00:14:23 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { JwtService } from './service/jwt.service';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +6,5 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  
+  title = 'edusched';
 }
